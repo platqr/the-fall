3351b4251460f5ae4b75820ce60f602a09f69573
platqr <miguelbuir@gmail.com>
2022-08-08 01:07:53 -0700

    lives and score

--- Assets/Scripts/GameMgmt.cs
+++ Assets/Scripts/GameMgmt.cs
@@ -1,28 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameMgmt : MonoBehaviour
 {
 
   private GameObject player;
   private Player playerScript;
-  private float time = 0;
+  private TMP_Text scoreText;
+  private TMP_Text livesText;
 
+  private float time = 0;
+  
   private int score = 0;
+  private int lives = 5;
   private float scoreTimer = 0f;
   private float scoreCoolDown = .5f;
+
+  private float livesTimer = 0f;
+  private float livesCoolDown = .5f;
 
   private void Awake() {
     player = GameObject.Find("Player");
     playerScript = player.GetComponent<Player>();
+    scoreText = GameObject.Find("Score Text").GetComponent<TMP_Text>();
+    livesText = GameObject.Find("Lives Text").GetComponent<TMP_Text>();
   }
 
   private void Update()
   {
     time += Time.deltaTime;
-    Reset();
     Score();
+    Death();
   }
 
   public void Score()
@@ -31,13 +41,16 @@ public class GameMgmt : MonoBehaviour
     {
       score++;
       scoreTimer = time;
-      Debug.Log(score);
+      scoreText.text = "Score: " + score;
     }
   }
 
-  private void Reset() {
-    if (Input.GetKeyDown(KeyCode.R))
+  private void Death() {
+    if (player.transform.position.z >= 20f && (time - livesTimer) >= livesCoolDown)
     {
+      lives--;
+      livesTimer = time;
+      livesText.text = "Lives: " + lives;
       playerScript.Reset();
     }
 	}
